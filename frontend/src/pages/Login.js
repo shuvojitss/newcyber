3b652edca10f62e7e219608a7511f3d119d159ef
Shuvojit Samanta <142198651+shuvojitss@users.noreply.github.com>
2026-03-24 12:33:33 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -406,7 +406,7 @@ const Login = ({ setUserProfile }) => {
 
           {/* Social Login Buttons */}
           <div className={`social-buttons form-element ${formFadeOut ? 'fade-out' : ''}`}>
-            <button type="button" className="social-btn" onClick={() => window.location.href = 'http://localhost:8000/auth/google'}>
+            <button type="button" className="social-btn" onClick={() => window.location.href = 'https://backendserver-rp1l.onrender.com/auth/google'}>
               <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92a5.06 5.06 0 0 1-2.2 3.32v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.1z" fill="#4285F4" />
                 <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853" />
@@ -416,7 +416,7 @@ const Login = ({ setUserProfile }) => {
               Google
             </button>
 
-            <button type="button" className="social-btn" onClick={() => window.location.href = 'http://localhost:8000/auth/github'}>
+            <button type="button" className="social-btn" onClick={() => window.location.href = 'https://backendserver-rp1l.onrender.com/auth/github'}>
               <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="white">
                 <path d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12z" />
               </svg>
